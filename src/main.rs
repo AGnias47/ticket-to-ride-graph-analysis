6193b6c11874dcd7a39a1f61647dd9545985e20b
Andy Gnias <AGnias47@gmail.com>
2022-05-15 16:51:50 -0400

    Begin static function to parse JSON from file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod route;
 mod ticket;
 
 fn main() {
+    route::routes_from_file("mattgawarecki-ticket-to-ride/usa.routes.json");
     println!("Hello, world!");
 }
 
--- src/route.rs
+++ src/route.rs
@@ -1,8 +1,18 @@
 use crate::city::City;
+use std::fs;
+use std::collections::HashMap;
 
 pub struct Route {
     source: City,
     destination: City,
     distance: u8,
     connections: Vec<String>
+}
+
+pub fn routes_from_file(fpath: &str) -> HashMap<String, serde_json::Value> {
+    let routes = fs::read_to_string(fpath)
+    .expect("Unable to read file");
+    let routes_map: HashMap<String, serde_json::Value> = serde_json::from_str(&routes)
+    .expect("Unable to parse JSON");
+    return routes_map;
 }
