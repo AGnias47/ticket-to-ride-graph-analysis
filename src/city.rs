ad363308f3e9e9cb55e612c625e33cd106cab309
Andy Gnias <AGnias47@gmail.com>
2022-05-16 23:24:42 -0400

    JSON parsing failing but progress

--- src/city.rs
+++ src/city.rs
@@ -1,5 +1,7 @@
 use crate::route::Route;
+use serde::Deserialize;
 
+#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Hash)]
 pub struct City {
     name: String,
     routes: Vec<Route>
--- src/route.rs
+++ src/route.rs
@@ -1,18 +1,55 @@
 use crate::city::City;
-use std::fs;
+use serde::Deserialize;
 use std::collections::HashMap;
+use std::fs;
+use std::clone::Clone;
 
+#[derive(Debug, Eq, PartialEq, Clone, Deserialize, Hash)]
 pub struct Route {
     source: City,
     destination: City,
     distance: u8,
-    connections: Vec<String>
+    connections: Vec<String>,
 }
 
-pub fn routes_from_file(fpath: &str) -> HashMap<String, serde_json::Value> {
-    let routes = fs::read_to_string(fpath)
-    .expect("Unable to read file");
-    let routes_map: HashMap<String, serde_json::Value> = serde_json::from_str(&routes)
-    .expect("Unable to parse JSON");
-    return routes_map;
+#[derive(Deserialize)]
+struct L1 {
+    destination_city: HashMap<City, L2>,
+}
+
+#[derive(Deserialize)]
+struct L2 {
+    distance: u8,
+    connections: Vec<L3>,
+}
+
+#[derive(Deserialize, Clone)]
+struct L3 {
+    color: String,
+    locomotives: u8,
+    tunnels: u8,
+}
+
+pub fn routes_from_file(fpath: &str) -> HashMap<City, Vec<Route>> {
+    let route_file_as_string = fs::read_to_string(fpath).expect("Unable to read file");
+    let route_file_as_map: HashMap<City, L1> = serde_json::from_str(&route_file_as_string).unwrap();
+    let mut final_routes_map: HashMap<City, Vec<Route>> = HashMap::new();
+    for (starting_city, destinations) in &route_file_as_map {
+        let mut routes_for_city: Vec<Route> = Vec::new();
+        for (destination, l2) in &destinations.destination_city {
+            let mut colors: Vec<String> = Vec::new();
+            for c in l2.connections.clone() {
+                colors.push(c.color);
+            }
+            let route: Route = Route {
+                source: (*starting_city).clone(),
+                destination: (*destination).clone(),
+                distance: l2.distance,
+                connections: colors.clone()
+            };
+            routes_for_city.push(route);
+        }
+        final_routes_map.insert((*starting_city).clone(), routes_for_city);
+    }
+    return final_routes_map;
 }
