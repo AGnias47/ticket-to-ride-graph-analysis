1a389dee38dc9556ef671bdc1a23cf92412b61be
Andy Gnias <AGnias47@gmail.com>
2022-05-19 08:18:31 -0400

    Stable JSON parser

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 mod city;
 mod route;
 mod ticket;
-use std::collections::HashMap;
 
 fn main() {
-    let routes: HashMap<String, route::L1>  = route::routes_from_file("mattgawarecki-ticket-to-ride/usa.routes.json");
-    println!("---Cities---");
-    for (k, _) in &routes {
-        println!("{}", k);
-    }
-    let chicago: &HashMap<String, route::L2> = &routes.get("Chicago").unwrap().destination_city;
-    println!("---Destinations from Chicago---");
-    for (k, _) in chicago {
-        println!("{}", k);
-    }
-    let to_omaha: &route::L2 = chicago.get("Omaha").unwrap();
-    println!("---Data on Route to Omaha---");
-    println!("Distance: {}", to_omaha.distance);
-    print!("Connections: ");
-    for c in &to_omaha.connections {
-        println!("{} ", c.color);
-    }
+    route::demo();
 }
 
--- src/route.rs
+++ src/route.rs
@@ -1,66 +1,64 @@
 use serde::Deserialize;
-use serde_json::Value;
 use std::collections::HashMap;
 use std::fs;
 use std::clone::Clone;
 
 #[derive(Debug, Eq, PartialEq, Clone, Deserialize, Hash)]
 pub struct Route {
-    source: String,
-    destination: String,
-    distance: u8,
-    connections: Vec<String>,
+    pub source: String,
+    pub destination: String,
+    pub distance: u8,
+    pub connections: Vec<String>,
 }
 
 
 #[derive(Debug, Deserialize)]
 pub struct L1 {
     #[serde(flatten)]
-    pub destination_city: HashMap<String, L2>,
+    destination_city: HashMap<String, L2>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct L2 {
-    pub distance: u8,
-    pub connections: Vec<L3>,
+struct L2 {
+    distance: u8,
+    connections: Vec<L3>,
 }
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct L3 {
-    pub color: String,
+struct L3 {
+    color: String,
     locomotives: u8,
     tunnels: u8,
 }
 
-fn route_file_to_L0(fpath: &str) -> HashMap<String, L1>  {
+fn route_file_to_hashmap(fpath: &str) -> HashMap<String, L1>  {
     let route_file_as_string = fs::read_to_string(fpath).expect("Unable to read file");
     let data: HashMap<String, L1> = serde_json::from_str(&route_file_as_string).unwrap();
     return data;
 }
 
 pub fn routes_from_file(fpath: &str) -> HashMap<String, L1>  {
-    let route_file_as_map: HashMap<String, L1> = route_file_to_L0(fpath);
+    let route_file_as_map: HashMap<String, L1> = route_file_to_hashmap(fpath);
     return route_file_as_map;
-    /*
-    let mut final_routes_map: HashMap<String, Vec<Route>> = HashMap::new();
-    for (starting_city, destinations) in &route_file_as_map.routes {
-        println!("{}", starting_city);
-        let mut routes_for_city: Vec<Route> = Vec::new();
-        for (destination, l2) in &destinations.destination_city {
-            let mut colors: Vec<String> = Vec::new();
-            for c in l2.connections.clone() {
-                colors.push(c.color);
-            }
-            let route: Route = Route {
-                source: (*starting_city).clone(),
-                destination: (*destination).clone(),
-                distance: l2.distance,
-                connections: colors.clone()
-            };
-            routes_for_city.push(route);
-        }
-        final_routes_map.insert((*starting_city).clone(), routes_for_city);
+}
+
+
+pub fn demo() {
+    let routes: HashMap<String, L1>  = routes_from_file("mattgawarecki-ticket-to-ride/usa.routes.json");
+    println!("---Cities---");
+    for (k, _) in &routes {
+        println!("{}", k);
     }
-    return final_routes_map;
-    */
+    let chicago: &HashMap<String, L2> = &routes.get("Chicago").unwrap().destination_city;
+    println!("---Destinations from Chicago---");
+    for (k, _) in chicago {
+        println!("{}", k);
+    }
+    let to_omaha: &L2 = chicago.get("Omaha").unwrap();
+    println!("---Data on Route to Omaha---");
+    println!("Distance: {}", to_omaha.distance);
+    print!("Connections: ");
+    for c in &to_omaha.connections {
+        println!("{} ", c.color);
+    }
 }
